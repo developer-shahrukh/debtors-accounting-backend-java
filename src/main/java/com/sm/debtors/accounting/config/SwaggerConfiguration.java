1071ed6d6d417f7aa34fd57fd36f0706aaf0f136
developer-shahrukh <shahrukh4854@gmail.com>
2025-02-22 19:45:26 +0530

    some changes

--- src/main/java/com/sm/debtors/accounting/config/SwaggerConfiguration.java
+++ src/main/java/com/sm/debtors/accounting/config/SwaggerConfiguration.java
@@ -1,63 +1,18 @@
-package com.baeldung.swagger2boot.configuration;
+package com.sm.debtors.accounting.config;
 
-import java.util.Collections;
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.DocExpansion;
-import springfox.documentation.swagger.web.ModelRendering;
-import springfox.documentation.swagger.web.OperationsSorter;
-import springfox.documentation.swagger.web.TagsSorter;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
-
 @Configuration
 public class SwaggerConfiguration {
-
-    private ApiInfo apiInfo() {
-        return new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service",
-                new Contact("Umang Budhwar", "www.baeldung.com", "umangbudhwar@gmail.com"),
-                "License of API", "API license URL", Collections.emptyList());
-    }
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Debtor's API")
+                        .version("1.0")
+                        .description("API documentation for the Debtor's system"));
     }
-
-    /**
-     * SwaggerUI information
-     */
-
-    @Bean
-    UiConfiguration uiConfig() {
-        return UiConfigurationBuilder.builder()
-                .deepLinking(true)
-                .displayOperationId(false)
-                .defaultModelsExpandDepth(1)
-                .defaultModelExpandDepth(1)
-                .defaultModelRendering(ModelRendering.EXAMPLE)
-                .displayRequestDuration(false)
-                .docExpansion(DocExpansion.NONE)
-                .filter(false)
-                .maxDisplayedTags(null)
-                .operationsSorter(OperationsSorter.ALPHA)
-                .showExtensions(false)
-                .tagsSorter(TagsSorter.ALPHA)
-                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
-                .validatorUrl(null)
-                .build();
-    }
-
 }
